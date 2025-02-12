039aa9450364a4bf3078e94fa31ed5da5396a801
Eduardo Araujo Da Silva <eduardoaraujodasilva@protonmail.com>
2025-02-12 14:30:10 -0300

    Error handling for user input and creation of match-case for the Main Menu

--- libs/menu.py
+++ libs/menu.py
@@ -9,8 +9,32 @@ class Menu:
 
     def main_menu(self):
         console = Console()
-        n = 10
-        print(Panel("Imprime texto [red]formatado? {n}"))
-        print(Panel("0 - Exit \n1 - Book list \n2 - New book\n3 - Delete book", title="MAIN MENU"))
-        r = console.input("[green]> ")
-        print(r)
+        print(Panel("[yellow]BITBOOK :book:"))
+        print(Panel("0 - Exit \n1 - Book list \n2 - New book\n3 - Delete book\n4 - Update book\n5 - Help", title="MAIN MENU"))
+
+        while True:
+            menu_option = str(console.input("[green]> ")).strip()  # Removes extra spaces
+
+            if menu_option.isdigit() and 0 <= int(menu_option) <= 5:  # Checks if it's a number and within the range
+                menu_option = int(menu_option)  # Converts to integer
+                break  # Exits the loop if valid
+            else:
+                console.print("[red]Invalid input! Please enter numbers within the option range (0 - 5).[/red]")
+
+        print(f"Selected option: {menu_option}")  # Example output for verification
+        return menu_option
+
+    def book_list(self):
+        print(f"listing books...")
+
+    def new_book(self):
+        print(f"New books...")
+
+    def delete_book(self):
+        print(f"Delete books...")
+
+    def update_book(self):
+        print(f"Update books...")
+
+    def help(self):
+        print(f" Help...")
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from models.books import Book
 from libs.menu import Menu
 
-while True:
+execution = True
+
+while execution:
     menu = Menu()
-    menu.main_menu()
-    break
+
+    menu_option = menu.main_menu()
+
+    match (menu_option):
+        case 0:
+            execution = False
+        case 1:
+            menu.book_list()
+        case 2:
+            menu.new_book()
+        case 3:
+            menu.delete_book()
+        case 4:
+            menu.update_book()
+        case 5:
+            menu.help()
+        case _:
+            print("menu_option invalid")
